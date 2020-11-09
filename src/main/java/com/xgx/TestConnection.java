ccf2e588d903f44baba3f52424ac37d9d3fb79c8
xuguoxing <xuguoxing@chinadaas.com>
2020-11-09 19:27:20 +0800

    Initial commit

---
+++ src/main/java/com/xgx/TestConnection.java
@@ -0,0 +1,38 @@
+package com.xgx;
+
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * Description: <br/>
+ *
+ * @author: xgx <br/>
+ * date: 2020/11/5 15:05 <br/>
+ */
+public class TestConnection {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.facebook.presto.jdbc.PrestoDriver");
+        String url = "jdbc:presto://192.168.137.103:8080/mongodb/testpresto";
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        Connection connection = DriverManager.getConnection(url,properties);
+        Statement stmt = connection.createStatement();
+        ResultSet rs = null;
+        try {
+//            rs = stmt.executeQuery("show tables");
+            rs = stmt.executeQuery("select * from basic limit 1");
+            while (rs.next()) {
+//                System.out.println(rs.getString(1) + "--" + rs.getString(2));
+                System.out.println(rs.getString(1));
+            }
+            rs.close();
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            if(rs != null)
+                rs.close();
+            connection.close();
+        }
+    }
+}
